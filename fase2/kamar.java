484335d9b3eb8622819ee2478ac2b0b5fc4b570c
nguraharyaa <ngurahhharyaa@gmail.com>
2026-04-24 20:37:19 +0800

    Menambahkan code class kamar fase2

--- fase2/kamar.java
+++ fase2/kamar.java
@@ -1,1 +1,27 @@
+public class Kamar {
+    private int nomor;
+    private String tipe;
+    private int harga;
 
+    public Kamar(int nomor, String tipe, int harga) {
+        this.nomor = nomor;
+        this.tipe = tipe;
+        this.harga = harga;
+    }
+
+    public int getNomor() {
+        return nomor;
+    }
+
+    public String getTipe() {
+        return tipe;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public void tampilkanInfo() {
+        System.out.println(nomor + " - " + tipe + " - Rp." + harga);
+    }
+}
