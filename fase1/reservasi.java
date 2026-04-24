9e8f1bebc96a4fbd8869758e2ac8befc387361f5
Niniek Anjaswari <anjaswarininiek@gmail.com>
2026-04-24 17:06:19 +0800

    Reservasi

--- fase1/reservasi.java
+++ fase1/reservasi.java
@@ -1,1 +1,11 @@
+public class Reservasi {
+    int total;
 
+    Reservasi(int harga, int malam) {
+        total = harga * malam;
+    }
+
+    void tampilkanTotal() {
+        System.out.println("Total bayar: Rp." + total);
+    }
+}
