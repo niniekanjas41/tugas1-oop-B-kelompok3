652d6053760da04e9fbc61dc9958317afc748f66
Niniek Anjaswari <anjaswarininiek@gmail.com>
2026-04-24 21:01:12 +0800

    Pelanggan

--- fase2/pelanggan.java
+++ fase2/pelanggan.java
@@ -1,1 +1,22 @@
+public class Pelanggan {
+    private String nama;
+    private String noHP;
 
+    public Pelanggan(String nama, String noHP) {
+        this.nama = nama;
+        this.noHP = noHP;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getNoHP() {
+        return noHP;
+    }
+
+    public void tampilkanPelanggan() {
+        System.out.println("Nama : " + nama);
+        System.out.println("No HP: " + noHP);
+    }
+}
